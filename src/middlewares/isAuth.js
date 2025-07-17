3bcecdd3a4adda8e8123428f8b672d3f1aea3f0f
Johnbosco <johnboscoon@gmail.com>
2025-07-17 18:35:59 +0100

    Added Middleware function to the project

---
+++ src/middlewares/isAuth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+const isAuthenticated = (req, res, next) => {
+    try {
+        // Check for the authorization header
+        const authHeader = req.headers.authorization;
+        if(!authHeader) {
+            return res.status(401).json({message: 'Authentication failed: Authorization header missing'});
+        }
+
+        const token = authHeader.split(' ')[1];
+        if(!token) {
+            return res.status(401).json({message: 'Authentication failed: Token missing'});
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(!decoded) {
+            return res.staus(401).json({message: 'Authentication failed: Token verification failed'});
+        }
+
+        req.user = decoded;
+        next();
+
+    } catch (error) {
+        console.log('Authentication Error:', error);
+        return res.status(401).json({message: 'Internal Server Error'});
+    }
+}
+
+module.exports = { isAuthenticated };
