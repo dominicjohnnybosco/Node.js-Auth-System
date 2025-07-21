a3d5399ec33a901ca9d829b1041a5baaf3cd5fe6
Johnbosco <johnboscoon@gmail.com>
2025-07-21 16:48:30 +0100

    Imported the rental controller to the car.route router file

--- src/controllers/rental.controller.js
+++ src/controllers/rental.controller.js
@@ -3,6 +3,7 @@ const Car = require('../models/car.schema');
 const rentCar = async (req, res) => {
     const { carId } = req.params;
     const userId = req.user.id;
+    const { startDate, endDate } = req.body;
 
     try {
         // find the car by Id
@@ -25,8 +26,21 @@ const rentCar = async (req, res) => {
         car.isRented = true;
         car.rentedBy = userId;
         car.isAvailable = false;
+        car.startDate = startDate;
+        car.endDate = endDate;
+
+        // calculation for the amount the user is going to pay for the period of days they are renting the car
+        const millisecondsInDay = 1000 * 60 * 60 * 24;
+        const rentalDays = Math.ceil((endDate - startDate) / millisecondsInDay);
+
+        // amount to charge for each day the user is with the car
+        const dailyRate = 50000; // 50k per day
+        const totalPrice = rentalDays * dailyRate;
+
+        car.totalPrice = totalPrice;
+        car.carStatus = "pending";
         await car.save();
-        return res.status(200).json({message: 'Car Rented Successfully', car});
+        return res.status(200).json({message: `Car Rented Successfully for ${rentalDays} Days and charged ${totalPrice}`, car});
     } catch (error) {
         console.log(error);
         return res.status(500).json({message: 'Internal Server Error'});
--- src/routes/car.route.js
+++ src/routes/car.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {getAllCars, addCar, editCar, deleteCar, searchCar} = require('../controllers/admin.controller');
 const { isAuthenticated } = require('../middlewares/isAuth');
+const { rentCar } = require('../controllers/rental.controller');
 const router = express.Router();
 
 router.get('/get-all-cars', getAllCars);
@@ -8,5 +9,6 @@ router.get('/search-cars', searchCar);
 router.post('/add-car', isAuthenticated, addCar);
 router.put('/edit-car/:carId', isAuthenticated, editCar);
 router.delete('/delete-car/:carId', isAuthenticated, deleteCar);
+router.post('/rent-car/:carId', isAuthenticated, rentCar);
 
 module.exports = router;
